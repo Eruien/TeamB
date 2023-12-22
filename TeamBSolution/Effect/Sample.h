0aa386e9520f545713a78f881e796f77ab6cb929
acuevumadlf <cwbl3240@naver.com>
2023-12-22 18:07:02 +0900

    23.12.22

--- TeamBSolution/Effect/Sample.h
+++ TeamBSolution/Effect/Sample.h
@@ -1,10 +1,53 @@
 #pragma once
 #include "LCore.h"
-
+class LParticle
+{
+public:
+	bool     m_bLife = true;
+	TVector3 m_vPos;
+	TVector3 m_vSclae;
+	TVector3 m_vRotation;
+	float   m_fElapsedTimer = 0.0f;
+	float	m_fOffsetTime = 0.0f;
+	float	m_fAnimTimer = 1.0f;
+	int		m_iCurrentAnimIndex = 0;
+	std::basic_string<wchar_t>   m_szName;
+	int		m_iID=0;
+	int		m_iMaxFrame=0;
+	void Frame()
+	{
+		if (m_iMaxFrame <= 1 || !m_bLife) return;
+		m_fElapsedTimer += LGlobal::g_fGameTimer;
+		if (m_fElapsedTimer >= m_fOffsetTime)
+		{
+			m_iCurrentAnimIndex++;
+			if (m_iCurrentAnimIndex >= m_iMaxFrame)
+			{
+				m_iCurrentAnimIndex = 0;
+				m_bLife = false;
+			}
+			m_fElapsedTimer -= m_fOffsetTime;
+		}
+	}
+};
 class Sample : public LCore
 {
 public:
 	std::shared_ptr<LDebugCamera> m_DebugCamera = nullptr;
+	std::unique_ptr<LPlaneObj> m_pMapObj = nullptr;
+	
+	std::unique_ptr<LSpriteObj> m_pSpriteTexObj = nullptr;
+	std::unique_ptr<LSpriteObj> m_pSpriteUVObj = nullptr;
+	std::unique_ptr<LSpriteObj> m_pSpriteAirObj = nullptr;
+public:
+
+	std::map<int, LSpriteObj*>	m_SpriteList;
+	std::list<LParticle>		m_ParticleList;
+	
+	int   m_iSpriteIndex = 0;
+
+	int m_iTexIndex = 0;
+
 public:
 	bool Init();
 	bool Frame();
