0c433cc6d7aae7a3b0199c2f2fc6c3bebeef8d33
Eruien <87483479+Eruien@users.noreply.github.com>
2024-03-22 17:38:59 +0900

    소드 트레일 완료 캣멀론 정리 노이즈맵 럴프필요 한손검은 색 다르게

--- TeamBSolution/TheFinalKillingFloor/LTrail.h
+++ TeamBSolution/TheFinalKillingFloor/LTrail.h
@@ -4,6 +4,16 @@
 class LTrail : public LObject
 {
 public:
+	int m_iTrailVertexSize = 12;
+	int m_iTrailCountSize = 11;
+	int m_iVertexCount = 0;
+	float m_TimerStart = 1.0;
+	float m_TimerEnd = 0.015;
+	int m_iCatmullRomIndex[4] = { 0, };
+public:
+	void RenderTrail(TVector3* localSwordLow, TVector3* localSwordHigh, TMatrix* matSwordParent);
+	void InterpolRenderTrail(TVector3* localSwordLow, TVector3* localSwordHigh, TMatrix* matSwordParent);
+public:
 	bool CreateVertexData() override;
 	bool CreateIndexData() override;
 };
--- TeamBSolution/TheFinalKillingFloor/SelectScene.h
+++ TeamBSolution/TheFinalKillingFloor/SelectScene.h
@@ -22,15 +22,10 @@ public:
 	std::shared_ptr<LWeapon> m_OneHandSword = nullptr;
 	LSelect m_Select;
 	PlayerType m_playerType = PlayerType::GUN;
+	// SwordTrail
 	LTrail* m_SwordTrail = nullptr;
-	int m_Height = 78;
-	int m_TopHeight = 255;
 	TVector3 LocalSwordLow = { 0.0f, 0.0f, 0.0f };
 	TVector3 LocalSwordHigh = { 0.0f, 0.0f, 255.0f };
-	int m_TrailVertexCount = 9;
-	int m_TimerCount = 0;
-	float m_TimerStart = 0.0;
-	float m_TimerEnd = 0.1;
 public:
 	// 처음 카메라 세팅
 	TVector3 m_BindCameraPos = { -28.948f, 51.073f, 99.283f };
@@ -62,8 +57,10 @@ public:
 	void InitializeWeapon();
 	void UpdateWeaponPosition();
 	void InitializeLighting();
+	void InitializeSwordTrail();
 	ID3D11Buffer* CreateConstantBuffer(ID3D11Device* pd3dDevice, void* data, UINT iNumIndex, UINT iSize, bool bDynamic = false);
 	void UpdateOBB();
+	void InterpolRenderTrail();
 public:
 	SelectScene(LScene* parent);
 	virtual ~SelectScene();
